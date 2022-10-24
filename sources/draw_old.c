3d2103a92043606c981e07aa14788b8751ee4f13
Hyojeong Lee <hyojlee@c7r7s4.42seoul.kr>
2022-10-24 21:30:42 +0900

    Fix: draw wall

--- sources/draw_old.c
+++ sources/draw_old.c
@@ -6,7 +6,7 @@
 /*   By: hyojlee <hyojlee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 15:56:52 by hyojlee           #+#    #+#             */
-/*   Updated: 2022/10/24 21:21:10 by hyojlee          ###   ########.fr       */
+/*   Updated: 2022/10/24 18:49:32 by hyojlee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,7 @@ void	chk_hit(t_info *info)
 	vec = &(info->vec);
 	while (hit == 0)
 	{
-		if (vec->side_dist.x < vec->side_dist.y) // 기울기가 1보다 큼
+		if (vec->side_dist.x < vec->side_dist.y)
 		{
 			vec->side_dist.x += vec->delta_dist.x;
 			vec->map.x += vec->step.x;
@@ -65,35 +65,29 @@ void	chk_hit(t_info *info)
 		vec->perp_wall_dist = (vec->map.y - vec->pos.y + (1 - vec->step.y) / 2) / vec->rayDir.y; 
 }
 
-void	set_draw_info(t_draw *draw, t_vector *vec)
-{
-	draw->line_hei = (int)(HEI / vec->perp_wall_dist);
-	draw->draw_start = (HEI / 2) - (draw->line_hei / 2);
-	if (draw->draw_start < 0)
-		draw->draw_start = 0;
-	draw->draw_end = (HEI / 2) + (draw->line_hei / 2);
-	if (draw->draw_end >= HEI)
-		draw->draw_end = HEI - 1;
-	if (vec->side == 0 && vec->step.x == 1)
-		draw->wall_idx = EA;
-	else if (vec->side == 0 && vec->step.x == -1)
-		draw->wall_idx = WE;
-	else if (vec->side == 1 && vec->step.y == 1)
-		draw->wall_idx = SO;
-	else
-		draw->wall_idx = NO;
-
-}
-
 void	draw_line(t_info *info)
 {
-	set_draw_info(&(info->draw), &(info->vec));
+	info->draw.line_hei = (int)(HEI / info->vec.perp_wall_dist);
+	info->draw.draw_start = (HEI / 2) - (info->draw.line_hei / 2);
+	if (info->draw.draw_start < 0)
+		info->draw.draw_start = 0;
+	info->draw.draw_end = (HEI / 2) + (info->draw.line_hei / 2);
+	if (info->draw.draw_end >= HEI)
+		info->draw.draw_end = HEI - 1;
+	if (info->vec.side == 0 && info->vec.step.x == 1)
+		info->draw.wall_idx = 3;
+	else if (info->vec.side == 0 && info->vec.step.x == -1)
+		info->draw.wall_idx = 2;
+	else if (info->vec.side == 1 && info->vec.step.y == 1)
+		info->draw.wall_idx = 1;
+	else
+		info->draw.wall_idx = 0;
 	if (info->vec.side == 0)
 		info->draw.wallX = info->vec.pos.y + info->vec.perp_wall_dist * info->vec.rayDir.y;
 	else
 		info->draw.wallX = info->vec.pos.x + info->vec.perp_wall_dist * info->vec.rayDir.x;
 	info->draw.wallX -= floor(info->draw.wallX);
-	info->draw.tex.x = (int)(info->draw.wallX * (double)TEX_WID);
+	info->draw.tex.x = (int)(info->draw.wallX * TEX_WID);
 	if (info->vec.side == 0 && info->vec.rayDir.x > 0)
 		info->draw.tex.x = TEX_WID - info->draw.tex.x - 1;
 	if (info->vec.side == 1 && info->vec.rayDir.y < 0)
@@ -114,46 +108,11 @@ void draw(int x, t_info *info)
 		info->draw.tex.y = (int)info->draw.tex_pos & (TEX_HEI - 1);
 		info->draw.tex_pos += info->draw.step;
 		get = info->map.imgs[info->draw.wall_idx].addr + (info->draw.tex.y * info->map.imgs[info->draw.wall_idx].line_len
-			+ info->draw.tex.x * info->map.imgs[info->draw.wall_idx].bpp / 8);
+			+ info->map.imgs[info->draw.wall_idx].bpp / 8);
 		info->draw.color = *(unsigned int *)get;
 		dest = info->img.addr + (y * info->img.line_len + x * (info->img.bpp / 8));
 		*(unsigned int *)dest = info->draw.color;
 		y++;
-	}
-}
-
-void	set_vector(int x, t_vector *vec)
-{
-	vec->cameraX = 2 * x / (double)WID - 1;
-	vec->rayDir.x = vec->dir.x + vec->plane.x * vec->cameraX;
-	vec->rayDir.y = vec->dir.y + vec->plane.y * vec->cameraX;
-	vec->map.x = (int)vec->pos.x;
-	vec->map.y = (int)vec->pos.y;
-	vec->delta_dist.x = fabs(1 / vec->rayDir.x);
-	vec->delta_dist.y = fabs(1 / vec->rayDir.y);
-}
-
-void	set_step(t_vector *vec)
-{
-	if (vec->rayDir.x < 0)
-	{
-		vec->step.x = -1;
-		vec->side_dist.x = (vec->pos.x - vec->map.x) * vec->delta_dist.x;
-	}
-	else
-	{
-		vec->step.x = 1;
-		vec->side_dist.x = (vec->map.x + 1 - vec->pos.x) * vec->delta_dist.x;
-	}
-	if (vec->rayDir.y < 0)
-	{
-		vec->step.y = -1;
-		vec->side_dist.y = (vec->pos.y - vec->map.y) * vec->delta_dist.y;
-	}
-	else
-	{
-		vec->step.y = 1;
-		vec->side_dist.y = (vec->map.y + 1 - vec->pos.y) * vec->delta_dist.y;
 	}
 }
 
@@ -166,8 +125,33 @@ void	dda(t_info *info)
 	vec = &(info->vec);
 	while (x < WID)
 	{
-		set_vector(x, vec);
-		set_step(vec);
+		vec->cameraX = 2 * x / (double)WID - 1;
+		vec->rayDir.x = vec->dir.x + vec->plane.x * vec->cameraX;
+		vec->rayDir.y = vec->dir.y + vec->plane.y * vec->cameraX;
+		vec->map.x = (int)vec->pos.x;
+		vec->map.y = (int)vec->pos.y;
+		vec->delta_dist.x = fabs(1 / vec->rayDir.x);
+		vec->delta_dist.y = fabs(1 / vec->rayDir.y);
+		if (vec->rayDir.x < 0)
+		{
+			vec->step.x = -1;
+			vec->side_dist.x = (vec->pos.x - vec->map.x) * vec->delta_dist.x;
+		}
+		else
+		{
+			vec->step.x = 1;
+			vec->side_dist.x = (vec->map.x + 1 - vec->pos.x) * vec->delta_dist.x;
+		}
+		if (vec->rayDir.y < 0)
+		{
+			vec->step.y = -1;
+			vec->side_dist.y = (vec->pos.y - vec->map.y) * vec->delta_dist.y;
+		}
+		else
+		{
+			vec->step.y = 1;
+			vec->side_dist.y = (vec->map.y + 1 - vec->pos.y) * vec->delta_dist.y;
+		}
 		chk_hit(info);
 		draw_line(info);
 		draw(x, info);
