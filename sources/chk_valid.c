d260f5b3066a84d44c47d5b2015ff510c3fbfa29
Hyojeong Lee <hyojlee@c8r5s9.42seoul.kr>
2022-10-27 15:26:28 +0900

    Enhance: fix error message when the map is not closed.

--- sources/chk_valid.c
+++ sources/chk_valid.c
@@ -6,7 +6,7 @@
 /*   By: hyojlee <hyojlee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 13:14:55 by hyojlee           #+#    #+#             */
-/*   Updated: 2022/10/27 15:02:46 by hyojlee          ###   ########.fr       */
+/*   Updated: 2022/10/27 15:25:57 by hyojlee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 
 static void	chk_surround(t_map *map, int i, int j)
 {
-	if (i == 0 || i == map->hei - 1 
+	if (i == 0 || i == map->hei - 1
 		|| j == 0 || j == map->wid - 1)
 	{
 		free_malloc(map->map, map->hei);
-		error("Invalid map", 0);
+		error("The map must be closed.", 0);
 	}
 	if ((i > 0 && BLANK == map->map[i - 1][j])
 		|| (i + 1 < map->hei && BLANK == map->map[i + 1][j])
@@ -26,7 +26,7 @@ static void	chk_surround(t_map *map, int i, int j)
 		|| (j + 1 < map->wid && BLANK == map->map[i][j + 1]))
 	{
 		free_malloc(map->map, map->hei);
-		error("Invalid map", 0);
+		error("The map must be closed.", 0);
 	}
 }
 
@@ -64,7 +64,7 @@ void	init_dir_vec(t_info *info)
 	info->vec.dir.y = 0;
 	info->vec.plane.x = 0;
 	info->vec.plane.y = 0;
-	if ('N'== info->map.dir)
+	if ('N' == info->map.dir)
 	{
 		info->vec.dir.y = -1;
 		info->vec.plane.x = 0.66;
